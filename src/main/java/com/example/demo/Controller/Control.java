32e4ba9879a1646abb236bdb583addb54b43b079
Felipe <tolentinoespinozaluisfelipe@gmail.com>
2025-07-02 03:14:31 -0500

    calcular tiempo

--- src/main/java/com/example/demo/Controller/Control.java
+++ src/main/java/com/example/demo/Controller/Control.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.Duration;
 import java.time.LocalTime;
 import java.util.List;
 
@@ -31,10 +32,42 @@ public class Control {
 		mav.addObject("esta",estacionService.getInfoEstaciones());
 		mav.addObject("horas",horTrenService.findAllHorarioTren());
 		mav.addObject("zonas",sonaService.findAllZonaTuristica());
-
+		t();
 		return mav;
 	}
 
+
+	public  void t(){
+		List<HorarioTren> h = horTrenService.findAllHorarioTren();
+		int Des = 10;// estacion donde quiero ir
+		int ori = 3; // estacion donde estoy
+		int direc = 2 ;
+		LocalTime now = LocalTime.now();
+		LocalTime abor = null;
+		LocalTime tf = null;
+		System.out.println(now);
+
+		for (HorarioTren ht : h) {
+			LocalTime hora = LocalTime.parse(ht.getHora());
+			int ubi = ht.getEstacionUbi();
+			int dic = ht.getDireccion();
+			int est = ht.getEstado();
+			if(Des>ori)     direc=1;
+
+			if(hora.isAfter(now) && ori == ubi  && dic == direc && est == 2) {
+
+				abor = hora;
+			}
+			if(abor != null && ubi == Des && est == 2){
+				tf = hora;
+				Duration dur = Duration.between(now, abor);
+				System.out.println(" tiempo hasta el abordaje :  "+dur.getSeconds()+"  - id: "+ht.getId());
+				dur = Duration.between(abor, tf);
+				System.out.println("tiempo estimado entre estaciones :   "+dur.getSeconds()+" -  id: "+ht.getId());
+				return;
+			}
+		}
+	}
 
 
 
